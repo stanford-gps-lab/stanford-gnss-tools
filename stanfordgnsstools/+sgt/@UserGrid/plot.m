cce07838d788e2dc83d5efe4b55d396487d5eba0
Andrew Neish <amneish@stanford.edu>
2019-07-31 17:36:16 -0700

    Added functional plot function to sgt.UserGrid

--- stanfordgnsstools/+sgt/@UserGrid/plot.m
+++ stanfordgnsstools/+sgt/@UserGrid/plot.m
@@ -4,32 +4,67 @@ function plot(obj, varargin)
 %   userGrid.plot(varargin) plots the users contained within userGrid on a
 %   map of the earth. The default map is a spherical globe.
 %
-%   See Also: sgt.User, sgt.UserGrid
+%   See Also: sgt.User, sgt.UserGrid, sgt.UserGrid.createUserGrid
 %
 %   varargin:
 %   -----
 %   'PolygonFile' - a polygon file that contains a set of points that
 %   define a geographic boundary.
+%   -----
+%   'Globe3D' - plots the userGrid and other information on a 3D globe
+
+% Copyright 2019 Stanford University GPS Laboratory
+%   This file is part of the Stanford GNSS Tools which is released under
+%   the MIT License. See `LICENSE.txt` for full license details.
+%   Questions and comments should be directed to the project at:
+%   https://github.com/stanford-gps-lab/stanford-gnss-tools
 
 % Parse varargin
 if (nargin > 1)
     res = parseInput(varargin{:});
     
-    if (~isempty(res.PolygonFile))
-        polygon = sgt.tools.generatePolygon(res.PolygonFile);
+    if (res.Globe3D)
+        figure;
+        % Plot map
+        sgt.plotearth.earth_sphere('m'); hold on;
+        % Plot user locations on map
+        plot3(obj.GridPositionECEF(:,1), obj.GridPositionECEF(:,2), obj.GridPositionECEF(:,3), 'r.', 'MarkerSize', 10)
+        % Plot polygon if requested
+        if (~isempty(res.PolygonFile))
+            polygonLLH = load(res.PolygonFile);
+            polygonECEF = sgt.tools.llh2ecef([polygonLLH(:,1),...
+                polygonLLH(:,2), 2e5*ones(length(polygonLLH),1)]);
+            fill3(polygonECEF(:,1), polygonECEF(:,2), polygonECEF(:,3), 'y', 'FaceAlpha', 0.5)
+        end
+        
+    else
+        figure;
+        % Plot map
+        coastData = load('coast');
+        % Plot user locations on map
+        plot(coastData.long, coastData.lat, 'k'); hold on;
+        plot(obj.GridPositionLLH(:,2), obj.GridPositionLLH(:,1), 'k.')
+        % Plot polygon if requested
+        if (~isempty(res.PolygonFile))
+            polygon = sgt.tools.generatePolygon(res.PolygonFile);
+            plot(polygon);
+            
+            inBound = [obj.Users(:).InBound];
+            if (sum(inBound) > 0)
+                plot(obj.GridPositionLLH(inBound, 2), obj.GridPositionLLH(inBound, 1), 'r.')
+            end
+        end
     end
+else
+    % Plot a 2D plot of the users on earth
+    figure;
+    % Plot map
+    coastData = load('coast');
+    % Plot user locations on map
+    plot(coastData.long, coastData.lat, 'k'); hold on;
+    plot(obj.GridPositionLLH(:,2), obj.GridPositionLLH(:,1), 'k.')
+    
 end
-
-% Plot map
-sgt.plotearth.earth_sphere('m');
-
-% Plot user locations on map
-hold on
-plot3(obj.GridPositionECEF(:,1), obj.GridPositionECEF(:,2), obj.GridPositionECEF(:,3), 'r.')
-
-
-
-
 end
 
 % Parse varargin
@@ -40,6 +75,10 @@ parser = inputParser;
 % PolygonFile
 validPolygonFileFn = @(x) (ischar(x));
 parser.addParameter('PolygonFile', [], validPolygonFileFn)
+
+% Globe3D
+validGlobe3DFn = @(x) (islogical(x));
+parser.addParameter('Globe3D', false, validGlobe3DFn)
 
 % Run parser and set results
 parser.parse(varargin{:})
