388d8fdfba552882f3466d52173d4ab359024a39
Andrew Neish <amneish@stanford.edu>
2019-08-02 15:01:37 -0700

    Beginning to make function to plot sky plots from user observations

---
+++ stanfordgnsstools/+sgt/@UserObservation/plotSkyPlot.m
@@ -0,0 +1,29 @@
+function plotSkyPlot(obj, satellites, time)
+% plotSkyPlot  Plot the skyplot for a UserObservation (obj) using specified
+%   satellites (satellites) at one or multiple times (time).
+%
+%   sgt.UserObservation.plotSkyPlot(obj, satellites, time) plots the 
+%   skyplot for a user using the specified satellites. If a single time is 
+%   specified, it shows an instantaneous skyplot. If more than one time is 
+%   shown, the skyplot will show a trace of the satellite motion through 
+%   the sky.
+%
+%   See Also: sgt.User, sgt.Satellite, sgt.SatellitePosition,
+%   sgt.Satellite.getPosition, sgt.UserObservation
+
+% Copyright 2019 Stanford University GPS Laboratory
+%   This file is part of the Stanford GNSS Tools which is released under 
+%   the MIT License. See `LICENSE.txt` for full license details.
+%   Questions and comments should be directed to the project at:
+%   https://github.com/stanford-gps-lab/stanford-gnss-tools
+
+% Have a UserObservation object
+
+% Get satellitePositions at the times specified in time.
+
+% Find the azimuth and elevation values of the satellites in view
+
+% Plot the satellite positions on a polar plot
+
+
+end
