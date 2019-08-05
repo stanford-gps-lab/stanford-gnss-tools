3390cecae874943ce447334aafff87508d3f7bf9
Andrew Neish <amneish@stanford.edu>
2019-08-05 12:47:43 -0700

    Added test parameters

--- Debug Files/debugScripts/testPlotSkyPlot.m
+++ Debug Files/debugScripts/testPlotSkyPlot.m
@@ -5,9 +5,16 @@ testResults = [];
 userGrid = sgt.UserGrid.createUserGrid('NumUsers', 100);
 equatorialUser = userGrid.Users(42);
 polarUser = userGrid.Users(end);
+satellites = sgt.Satellite.fromYuma('current.alm');
+time = 0;
 
 
 %% Test 1 - Basic test of plotting a skyplot
+try
+    equatorialUser.plotSkyPlot(satellites, time);
+catch
+    testResults(1) = 1;
+end
 
 %% Display test results
 if any(testResults)
