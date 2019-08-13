bd336da6900335da9644445a8e03e23f3846cf27
Andrew Neish <amneish@stanford.edu>
2019-08-13 09:44:44 -0700

    Modified outputs from testAll.m

--- Debug Files/testAll.m
+++ Debug Files/testAll.m
@@ -18,7 +18,7 @@ for i = 1:length(testList)
     run(fullfile(testDir, testList(i).name));
 end
 
-fprintf('\n\nLicenses in Use:\n')
+fprintf('\nLicenses in Use:\n')
 license('inuse') %  This helps in development to ensure that on MATLAB is being used.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
