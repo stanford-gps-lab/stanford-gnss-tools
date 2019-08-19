def0ba5b29f95a3abaffca73c6d876b658018a4c
Andrew Neish <amneish@stanford.edu>
2019-08-19 13:48:25 -0700

    Modified ecef2enu.m

--- stanfordgnsstools/+sgt/+tools/ecef2enu.m
+++ stanfordgnsstools/+sgt/+tools/ecef2enu.m
@@ -1,24 +1,17 @@
 function ecef2enuMat = ecef2enu(posLLH)
 % ecef2enu(posLLH) returns the rotation matrix from the ecef to enu frame.
-% posLLH is a Nx3 array [deg deg m] where N is the number of input
-% positions. ecef2enuMat is a 3x3xN matrix.
-
-% Number of positions
-[numPos, ~] = size(posLLH);
+% posLLH is a 1x3 array [deg deg m]. ecef2enuMat is a 3x3 matrix.
 
 % Convert to radians
-latRad = posLLH(:,1).*180/pi;
-lonRad = posLLH(:,2).*180/pi;
+latRad = posLLH(1).*180/pi;
+lonRad = posLLH(2).*180/pi;
 
 % Preallocate
-ecef2enuMat = zeros(3,3,numPos);
+ecef2enuMat = zeros(3,3);
 
-% Loop through the input positions
-for i = 1:numPos
-    ecef2enuMat(:,:,i) = [...
-        -sin(lonRad(i))         ,  cos(lonRad(i))         , 0;...
-        -sin(latRad(i))*cos(lonRad(i)), -sin(latRad(i))*sin(lonRad(i)), cos(latRad(i));...
-        cos(latRad(i))*cos(lonRad(i)),  cos(latRad(i))*sin(lonRad(i)), sin(latRad(i))...
+ecef2enuMat(:,:) = [...
+    -sin(lonRad), cos(lonRad), 0;...
+    -sin(latRad)*cos(lonRad), -sin(latRad)*sin(lonRad), cos(latRad);...
+    cos(latRad)*cos(lonRad),  cos(latRad)*sin(lonRad), sin(latRad)...
     ];
-end
 end
