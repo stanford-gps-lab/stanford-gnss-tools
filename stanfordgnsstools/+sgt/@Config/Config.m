1192f35b95511cce63ced13e2568fdcf547ccff1
Andrew Neish <amneish@stanford.edu>
2019-08-20 13:45:47 -0700

    Created a Config class to store configuration parameters

---
+++ stanfordgnsstools/+sgt/@Config/Config.m
@@ -0,0 +1,31 @@
+classdef Config < matlab.mixin.Copyable
+    % Config     a container for test configuration input parameters.
+    %
+    %    sgt.Config() creates an object that will store configuration
+    %    variables input to simulations. These inputs can be referenced in
+    %    the future and can clarify the inputs used in past simulations.
+    
+    % Copyright 2019 Stanford University GPS Laboratory
+    %   This file is part of the Stanford GNSS Tools which is released
+    %   under the MIT License. See `LICENSE.txt` for full license details.
+    %   Questions and comments should be directed to the project at:
+    %   https://github.com/stanford-gps-lab/stanford-gnss-tools
+    
+    % Public Properties
+    properties
+        % Satellite - Inputs to class Satellite
+        Satellite
+        
+    end
+    
+    % Constructor
+    methods
+        function obj = Config()
+            
+        end
+    end
+    
+    
+    
+    
+end
