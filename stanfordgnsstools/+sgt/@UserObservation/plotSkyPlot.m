f9c198c9fa3a7fe1b48ea3f8319a953ff9053d03
Andrew Neish <amneish@stanford.edu>
2019-08-28 09:11:38 -0700

    Fixed skyplot prnInView vs svInView due to PRN4 not included

--- stanfordgnsstools/+sgt/@UserObservation/plotSkyPlot.m
+++ stanfordgnsstools/+sgt/@UserObservation/plotSkyPlot.m
@@ -24,19 +24,19 @@ for i = 1:length(obj)
     % Plot the satellite positions on a polar plot
     temp = [obj(i).SatellitePosition.Satellite]; temp2 = [temp.PRN]';
     prnInView = temp2(obj(i).SatellitesInViewMask);
-    polarscatter(obj(i).AzimuthAngles, 90 - obj(i).ElevationAngles.*180/pi, sz, prnInView, 'filled');
+    svInView = obj(i).SatellitesInViewMask;
+    polarscatter(obj(i).AzimuthAngles(svInView), 90 - obj(i).ElevationAngles(svInView).*180/pi, sz, prnInView, 'filled');
     
     % Text to be added next to each point
     if length(obj) == 1
         textStr = cellstr(strcat('PRN ', num2str(prnInView)));
         dx = 0*pi/180; dy = 10;
-        text(obj(i).AzimuthAngles+dx, 90 - obj(i).ElevationAngles.*180/pi+dy, textStr)
+        text(obj(i).AzimuthAngles(svInView)+dx, 90 - obj(i).ElevationAngles(svInView).*180/pi+dy, textStr)
     end   
 end
 rlim([0 90])
 
 % Plot the elevation mask
 polarplot(linspace(0,2*pi,100), 90 - obj(1).User.ElevationMask*180/pi.*ones(100,1), 'b--')
-
 
 end
